13eb2fe02bbe31ed1bd19a860f59036d8dd7719b
Aaron Schlesinger <aaron@deis.com>
2016-10-04 15:26:47 -0700

    feat(mode): add async support to CF mode
    Fixes https://github.com/deis/steward/issues/175

--- mode/provision_response.go
+++ mode/provision_response.go
@@ -3,5 +3,6 @@ package mode
 // ProvisionResponse represents a response to a provisioning request. It is marked with JSON struct tags so that it can be encoded to, and decoded from the CloudFoundry provisioning response body format. See https://docs.cloudfoundry.org/services/api.html#provisioning for more details
 type ProvisionResponse struct {
 	Operation string     `json:"operation"`
+	IsAsync   bool       `json:"-"`
 	Extra     JSONObject `json:"extra,omitempty"`
 }
