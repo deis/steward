61511c64cbdbcfde65b78225769abc491010f41c
Aaron Schlesinger <aaron@deis.com>
2016-10-05 11:46:03 -0700

    fix(mode/cmd,mode/helm): use no-op last operation getter
    in helm and cmd mode

--- mode/cmd/lifecycler.go
+++ mode/cmd/lifecycler.go
@@ -2,14 +2,16 @@ package cmd
 
 import (
 	"github.com/deis/steward/mode"
+	"github.com/deis/steward/mode/common"
 )
 
 // newLifecycler returns a new mode.Lifecycler that's implemented with a backend cmd broker
 func newLifecycler(pr *podRunner) *mode.Lifecycler {
 	return &mode.Lifecycler{
-		Provisioner:   newProvisioner(pr),
-		Deprovisioner: newDeprovisioner(pr),
-		Binder:        newBinder(pr),
-		Unbinder:      newUnbinder(pr),
+		Provisioner:         newProvisioner(pr),
+		Deprovisioner:       newDeprovisioner(pr),
+		Binder:              newBinder(pr),
+		Unbinder:            newUnbinder(pr),
+		LastOperationGetter: common.NoopLastOperationGetter{},
 	}
 }
