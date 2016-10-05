6211f1aaaec9f68971de50cead6d83b50ed96d24
Aaron Schlesinger <aaron@deis.com>
2016-10-05 11:35:35 -0700

    fix(mode): implement get last operation response struct

--- mode/get_last_operation_response.go
+++ mode/get_last_operation_response.go
@@ -1,11 +1,49 @@
 package mode
 
-const (
-  StateSucceeded = "succeeded"
-  StateFailed = "failed"
-  StateInProgress = "in progress"
+import (
+	"fmt"
 )
 
+// ErrUnknownLastOperation is an error indicating that a string is not a valid known last operation
+type ErrUnknownLastOperation string
+
+// Error is the error interface implementation
+func (e ErrUnknownLastOperation) Error() string {
+	return fmt.Sprintf("unknown last operation '%s'", string(e))
+}
+
+// LastOperationState represents the state returned in a "get last operation" call. This type implements fmt.Stringer
+type LastOperationState string
+
+// String is the fmt.Stringer interface implementation
+func (l LastOperationState) String() string {
+	return string(l)
+}
+
+const (
+	// LastOperationStateSucceeded is the LastOperationState indicating that the operation has succeeded
+	LastOperationStateSucceeded LastOperationState = "succeeded"
+	// LastOperationStateFailed is the LastOperationState indicating that the operation has failed
+	LastOperationStateFailed LastOperationState = "failed"
+	// LastOperationStateInProgress is the LastOperationState indicating the the operation is still in progress
+	LastOperationStateInProgress LastOperationState = "in progress"
+)
+
+// GetLastOperationResponse is the response body from a get last operation call
 type GetLastOperationResponse struct {
-  State string `json:"state"`
+	State string `json:"state"`
+}
+
+// GetState returns the LastOperationState defined in g.State, or an error if g.State is not a valid LastOperationState
+func (g *GetLastOperationResponse) GetState() (LastOperationState, error) {
+	switch g.State {
+	case LastOperationStateSucceeded.String():
+		return LastOperationStateSucceeded, nil
+	case LastOperationStateFailed.String():
+		return LastOperationStateFailed, nil
+	case LastOperationStateInProgress.String():
+		return LastOperationStateInProgress, nil
+	default:
+		return LastOperationState(""), ErrUnknownLastOperation(g.State)
+	}
 }
