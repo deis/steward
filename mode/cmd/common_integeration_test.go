96a3f60625943768041769b3a738d823475edd46
Kent Rancourt <kent@deis.com>
2016-10-05 15:17:20 -0400

    chore(cmd mode): Add more integration tests

--- mode/cmd/common_integeration_test.go
+++ mode/cmd/common_integeration_test.go
@@ -6,29 +6,43 @@ import (
 	"os"
 	"testing"
 
+	"github.com/deis/steward/mode"
 	"github.com/deis/steward/test-utils/k8s"
 	testsetup "github.com/deis/steward/test-utils/setup"
 	"github.com/technosophos/moniker"
+	"k8s.io/client-go/1.4/kubernetes"
 )
 
-var namespace string
+var (
+	clientset      *kubernetes.Clientset
+	testCataloger  mode.Cataloger
+	testLifecycler *mode.Lifecycler
+	testNamespace  string
+)
 
 func TestMain(m *testing.M) {
 	testsetup.SetupAndTearDown(m, setup, teardown)
 }
 
 func setup() error {
-	namespace = moniker.New().NameSep("-")
-	if err := k8s.EnsureNamespace(namespace); err != nil {
+	var err error
+	if clientset, err = k8s.GetClientset(); err != nil {
 		return err
 	}
-	os.Setenv("POD_NAMESPACE", namespace)
+	testNamespace = moniker.New().NameSep("-")
+	if err := k8s.EnsureNamespace(testNamespace); err != nil {
+		return err
+	}
+	os.Setenv("POD_NAMESPACE", testNamespace)
 	os.Setenv("CMD_IMAGE", "quay.io/deisci/cmd-sample-broker:devel")
+	if testCataloger, testLifecycler, err = GetComponents(clientset); err != nil {
+		return err
+	}
 	return nil
 }
 
 func teardown() error {
-	if err := k8s.DeleteNamespace(namespace); err != nil {
+	if err := k8s.DeleteNamespace(testNamespace); err != nil {
 		return err
 	}
 	return nil
