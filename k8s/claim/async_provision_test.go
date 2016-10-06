34c590382c906785322c85b2cb80f8d2441e0558
Aaron Schlesinger <aaron@deis.com>
2016-10-06 13:10:26 -0700

    fix(k8s/claim/async_provision_test.go): fix deadlock

--- k8s/claim/async_provision_test.go
+++ k8s/claim/async_provision_test.go
@@ -2,6 +2,8 @@ package claim
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"testing"
 	"time"
 
@@ -13,6 +15,9 @@ import (
 )
 
 func TestPollProvisionState(t *testing.T) {
+	const (
+		timeout = 1 * time.Second
+	)
 	ctx, cancelFn := context.WithCancel(context.Background())
 	defer cancelFn()
 
@@ -20,16 +25,14 @@ func TestPollProvisionState(t *testing.T) {
 	planID := uuid.New()
 	operation := "testOperation"
 	instanceID := uuid.New()
+
+	var curStateMut sync.RWMutex
 	curState := mode.LastOperationStateInProgress
 
-	switcherCh := make(chan mode.LastOperationState)
 	lastOpGetter := &fake.LastOperationGetter{
 		Ret: func() *mode.GetLastOperationResponse {
-			select {
-			case newState := <-switcherCh:
-				curState = newState
-			default:
-			}
+			curStateMut.RLock()
+			defer curStateMut.RUnlock()
 			return &mode.GetLastOperationResponse{State: curState.String()}
 		},
 	}
@@ -40,24 +43,34 @@ func TestPollProvisionState(t *testing.T) {
 		assert.Equal(t, finalState, mode.LastOperationStateSucceeded, "final state")
 	}()
 
-	select {
-	case update := <-claimCh:
-		assert.Equal(t, update.Status(), mode.StatusProvisioningAsync, "claim update status")
-	case <-time.After(1 * time.Second):
-		t.Fatalf("got no initial update")
-	}
+	/////
+	// expect a provisioning-async first. after we receive it, the last op getter will get another provisioning-async and wait to send it. we then change the current state, receive the second provisioning-async and then expect the channel to not receive anymore. the final success state is received in the return value of pollProvisionState, and it's checked in the above goroutine
+	/////
 
-	// TODO: deadlock here
-	select {
-	case switcherCh <- mode.LastOperationStateSucceeded:
-	case <-time.After(1 * time.Second):
-		t.Fatalf("unable to switch the last operation state")
-	}
+	assert.NoErr(t, acceptStatus(claimCh, mode.StatusProvisioningAsync))
+
+	curStateMut.Lock()
+	curState = mode.LastOperationStateSucceeded
+	curStateMut.Unlock()
+
+	assert.NoErr(t, acceptStatus(claimCh, mode.StatusProvisioningAsync))
 
 	select {
 	case update := <-claimCh:
-		assert.Equal(t, update.Status(), mode.StatusProvisioned, "claim update status")
-	case <-time.After(1 * time.Second):
-		t.Fatalf("got no second update")
+		t.Fatalf("received %s on claim channel, expected nothing", update)
+	case <-time.After(timeout):
+	}
+}
+
+func acceptStatus(claimCh <-chan state.Update, expected mode.Status) error {
+	const timeout = 1 * time.Second
+	select {
+	case update := <-claimCh:
+		if update.Status() != expected {
+			return fmt.Errorf("expected status %s, got %s", expected, update.Status())
+		}
+		return nil
+	case <-time.After(timeout):
+		return fmt.Errorf("no status update after %s", timeout)
 	}
 }
